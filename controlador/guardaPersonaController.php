ddc7715b8e961d7378e37975fffe3a5424a8d014
Andres Benavides <andres.benavides@brm.com.co>
2015-08-26 17:10:03 -0500

    aprender angular

--- controlador/guardaPersonaController.php
+++ controlador/guardaPersonaController.php
@@ -1,11 +1,14 @@
 <?php
+
 include_once './db/Conexion.php';
 include_once './modelo/Persona.php';
+include_once './modelo/PersonaModelo.php';
 $bandas = implode(",", $data['bandas']);
-$autorizaInfo = (($data['autorizaInfo']==1))?'S':'N';
-$recibeInfo = (($data['recibeInfo']==1))?'S':'N';
+$autorizaInfo = (($data['autorizaInfo'] == 1 || isset($data['autorizaInfo']))) ? 'S' : 'N';
+$recibeInfo = (($data['recibeInfo'] == 1 || isset($data['autorizaInfo']))) ? 'S' : 'N';
 $persona = new Persona($data['nombre'], $data['genero'], $data['ciudad'], $bandas, $data['descripcion'], $autorizaInfo, $recibeInfo);
-$registro = $persona->guardarPersona();
-echo $registro;
-print_r($data);
+$persona->guardarPersona();
+$registro = new PersonaModelo();
+$registros = $registro->getRegistros();
+echo json_encode($registros);
 
--- js/controller.js
+++ js/controller.js
@@ -1,24 +1,24 @@
 var app = angular.module("formulario", []);
 
 app.controller("formController", function ($scope, $http) {
+  var consulta = this;
+  $scope.fRegistros = {action: 'consultar'};
+  $scope.registrosJson = [];
+  $http.post("index.php", $scope.fRegistros)
+          .success(function (data) {
+            $scope.registrosJson = eval(data);
+            console.log(data);
+          });
   var frmCtr = this;
   frmCtr.fDatos = {action: 'guardar'};
   frmCtr.enviar = function () {
     $http.post("index.php", frmCtr.fDatos)
             .success(function (data) {
+              $scope.registrosJson = eval(data);
               console.log(data);
             });
+    frmCtr.fDatos = {};
   };
-});
-
-app.controller("registros", function ($scope, $http) {
-  var consulta = this;
-  consulta.fDatos = {action: 'consultar'};
-  $scope.registrosJson = [];
-  $http.post("index.php", consulta.fDatos)
-          .success(function (data) {
-            $scope.registrosJson = data;
-            console.log(data);
-          });
 
 });
+
