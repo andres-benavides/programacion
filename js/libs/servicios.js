a69ec1bae4328e4eb2dc35f8fe39082ae7a3b2c2
brm-benavidesa <andres.benavides@brm.com.co>
2015-08-26 17:10:45 -0500

    servicios

--- js/libs/servicios.js
+++ js/libs/servicios.js
@@ -1,5 +1,10 @@
 (function(){
-  
+  angular.module('servicios',[])
+          .factory('guardarRegistros',['$http','$q','$window',function ($http,$q,$window){
+              function all(){
+                
+              }
+          }])
 })();
 
 
